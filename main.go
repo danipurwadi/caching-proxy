5467d4ffad6fa268c8dcc8a86180bf48f9353dbe
danipurwadi <dani.purwadi@lionsbot.com>
2024-10-11 23:22:21 +0800

    feat: add skeleton server and parse flag

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	portPtr := flag.Int("port", 3000, "port on which the caching proxy server will run")
+	originPtr := flag.String("origin", "", "URL of the server to which the requests will be forwarded")
+	flag.Parse()
+
+	if *originPtr == "" {
+		log.Fatal("failed to start program due to missing parameter: --origin <string>")
+	}
+
+	initServer(*portPtr, *originPtr)
+}
+
+func initServer(portNo int, origin string) {
+	log.Printf("starting cache-proxy at %d for origin %s...\n", portNo, origin)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Forwarding request to %s%s", origin, html.EscapeString(r.URL.Path))
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portNo), nil))
 }
