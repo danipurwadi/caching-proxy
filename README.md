c584133795cf7bf60a60b7085fcc1a218df42e59
danipurwadi <dani.purwadi@lionsbot.com>
2024-10-12 01:04:38 +0800

    feat: add caching for requests to same url

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # caching-proxy
-Project from roadmap.sh
+Project from https://roadmap.sh/projects/caching-server
 
 You are required to build a CLI tool that starts a caching proxy server, it will forward requests to the actual server and cache the responses. If the same request is made again, it will return the cached response instead of forwarding the request to the server.
 
--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html"
+	"io"
 	"log"
 	"net/http"
 )
+
+type cacheHttpResp struct {
+	statusCode int
+	respBody   []byte
+	headers    http.Header
+}
 
 func main() {
 	portPtr := flag.Int("port", 3000, "port on which the caching proxy server will run")
@@ -21,9 +27,53 @@ func main() {
 }
 
 func initServer(portNo int, origin string) {
+	cache := make(map[string]cacheHttpResp)
+
 	log.Printf("starting cache-proxy at %d for origin %s...\n", portNo, origin)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Forwarding request to %s%s", origin, html.EscapeString(r.URL.Path))
+		path := r.URL.Path
+
+		if cache[path].respBody == nil {
+			resp, err := http.Get(origin + path)
+			if err != nil {
+				log.Println("request to origin failed", err)
+				http.Error(w, "failed to get response", http.StatusInternalServerError)
+			}
+
+			defer resp.Body.Close()
+
+			for k, vs := range resp.Header {
+				for _, v := range vs {
+					w.Header().Set(k, v)
+				}
+			}
+			w.Header().Set("X-Cache", "MISS")
+			w.WriteHeader(resp.StatusCode)
+
+			respBytes, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("failed to parse response body", err)
+				http.Error(w, "failed to get response from source", http.StatusInternalServerError)
+			}
+
+			cache[path] = cacheHttpResp{
+				statusCode: resp.StatusCode,
+				respBody:   respBytes,
+				headers:    resp.Header,
+			}
+			w.Write(respBytes)
+		} else {
+			resp := cache[path]
+			for k, vs := range resp.headers {
+				for _, v := range vs {
+					w.Header().Set(k, v)
+				}
+			}
+			w.Header().Set("X-Cache", "HIT")
+			w.WriteHeader(resp.statusCode)
+
+			w.Write(resp.respBody)
+		}
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portNo), nil))
 }
