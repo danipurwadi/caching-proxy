514bb4c54a08344b02524796d22ffde7d6e4179c
danipurwadi <dani.purwadi@lionsbot.com>
2024-10-12 12:51:53 +0800

    refactor: abstract out writing response function

--- main.go
+++ main.go
@@ -10,20 +10,23 @@ import (
 
 type cacheHttpResp struct {
 	statusCode int
-	respBody   []byte
+	body       []byte
 	headers    http.Header
 }
 
 func main() {
-	portPtr := flag.Int("port", 3000, "port on which the caching proxy server will run")
-	originPtr := flag.String("origin", "", "URL of the server to which the requests will be forwarded")
+	var port int
+	var origin string
+
+	flag.IntVar(&port, "port", 3000, "port on which the caching proxy server will run")
+	flag.StringVar(&origin, "origin", "", "URL of the server to which the requests will be forwarded")
 	flag.Parse()
 
-	if *originPtr == "" {
+	if origin == "" {
 		log.Fatal("failed to start program due to missing parameter: --origin <string>")
 	}
 
-	initServer(*portPtr, *originPtr)
+	initServer(port, origin)
 }
 
 func initServer(portNo int, origin string) {
@@ -33,7 +36,7 @@ func initServer(portNo int, origin string) {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
-		if cache[path].respBody == nil {
+		if cache[path].body == nil {
 			resp, err := http.Get(origin + path)
 			if err != nil {
 				log.Println("request to origin failed", err)
@@ -41,15 +44,6 @@ func initServer(portNo int, origin string) {
 			}
 
 			defer resp.Body.Close()
-
-			for k, vs := range resp.Header {
-				for _, v := range vs {
-					w.Header().Set(k, v)
-				}
-			}
-			w.Header().Set("X-Cache", "MISS")
-			w.WriteHeader(resp.StatusCode)
-
 			respBytes, err := io.ReadAll(resp.Body)
 			if err != nil {
 				log.Println("failed to parse response body", err)
@@ -58,22 +52,26 @@ func initServer(portNo int, origin string) {
 
 			cache[path] = cacheHttpResp{
 				statusCode: resp.StatusCode,
-				respBody:   respBytes,
+				body:       respBytes,
 				headers:    resp.Header,
 			}
-			w.Write(respBytes)
+			resp.Header["X-Cache"] = []string{"MISS"}
+			writeResp(w, resp.StatusCode, resp.Header, respBytes)
 		} else {
 			resp := cache[path]
-			for k, vs := range resp.headers {
-				for _, v := range vs {
-					w.Header().Set(k, v)
-				}
-			}
-			w.Header().Set("X-Cache", "HIT")
-			w.WriteHeader(resp.statusCode)
-
-			w.Write(resp.respBody)
+			resp.headers["X-Cache"] = []string{"HIT"}
+			writeResp(w, resp.statusCode, resp.headers, resp.body)
 		}
 	})
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", portNo), nil))
+}
+
+func writeResp(w http.ResponseWriter, statusCode int, headers map[string][]string, body []byte) {
+	for k, vs := range headers {
+		for _, v := range vs {
+			w.Header().Set(k, v)
+		}
+	}
+	w.WriteHeader(statusCode)
+	w.Write(body)
 }
